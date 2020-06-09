5c74c2f4d5f361648915072524abaec1f60e1385
JohnDanielKim <danieljohnkim@ucla.edu>
2020-06-09 16:19:29 -0700

    Initial Commit

---
+++ hw2-gradecalc/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  hw2-gradecalc
+//
+//  Created by Daniel Kim on 6/9/20.
+//  Copyright © 2020 John Kim. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+	// insert code here...
+	std::cout << "Hello, World!\n";
+	return 0;
+}
